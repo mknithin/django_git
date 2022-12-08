7c0f16b78e935b61e42e3e40565ebb09f57bb96d
mknithin <sendtonithin@gmail.com>
2022-12-08 16:20:19 +0400

    added comments in urls

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
+# Django urls are always awesome
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
